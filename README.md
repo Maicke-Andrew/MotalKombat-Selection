6f06efa917ab64ea5e648e298c83f6b9e3b7318d
Maicke Ribeiro <maicke.ribeiro@impar.com.br>
2022-05-19 03:20:13 -0300

    criação do readme

---
+++ README.md
@@ -0,0 +1,4 @@
+### Projeto 'Marvel Player Select'
+
+Projeto em conjunto com 'Dev Em Dobro' em uma Dev Week, o proposto era fazer uma tela de selecionar personagem utilizando HTML, CSS, JavaScript.
+Foi abordado bastante sobre HTML, CSS e Responsividade, um ótimo aprendizado, e mais algumas dicas de estudos para responsividade.
--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,20 @@
 const characters = document.querySelectorAll('.character')
 
-nextPlayer = false
+characters.forEach((character) => {
 
-
-if (nextPlayer === false) {
-    
-    //TERMINA FOREACH
-    characters.forEach((character) => {
-        //TERMINA FOREACH
-        //TERMINA EVENTLISTENER
     character.addEventListener('mouseenter', () => {
         const selectedCharacter = document.querySelector('.selected')
         selectedCharacter.classList.remove('selected')
         character.classList.add('selected')
-        
+
         const imageFirstPlayer = document.getElementById('first-player')
         imageFirstPlayer.src = `./src/assets/${character.id}.png`
-        
+
         const firstName = document.getElementById('first-character-name')
         const nameCharacter = character.getAttribute('data-name');
-        
+
         firstName.innerHTML = `${nameCharacter}`
-        //TERMINA if ELSE   
+
         if (character.id === 'scorpion') {
             imageFirstPlayer.style.height = '140%';
             imageFirstPlayer.style.top = '38%';
@@ -31,18 +24,6 @@ if (nextPlayer === false) {
         } else {
             imageFirstPlayer.style.height = '';
             imageFirstPlayer.style.top = '';
-            //TERMINA if ELSE
         };
-        character.addEventListener('click', () => nextPlayer = true)
-        //TERMINA if ELSE
-        //TERMINA EVENTLISTENER
     });
-    //TERMINA EVENTLISTENER
-    //TERMINA FOREACH
-return nextPlayer = true
 });
-
-//TERMINA FOREACH
-} else {
-    alert(A)
-}
